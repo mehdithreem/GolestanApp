b350dcce5a4e04b83f29a6e5fb0b1b8405bed352
mehdithreem <mehdithreem@gmail.com>
2017-05-20 15:03:59 +0430

    Condition

---
+++ src/main/java/com/golestan/app/domain/Course/Course.java
@@ -0,0 +1,26 @@
+package com.golestan.app.domain.Course;
+
+import com.golestan.app.domain.Policy;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by mehdithreem on 5/20/2017 AD.
+ */
+@Entity
+@Table( name="Course")
+public class Course {
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="COURSE_UNIT")
+    private Integer courseUnit;
+
+    @Column(name="LESSON_UNIQUE_ID")
+    private Integer lessonUniqueId;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Policy> policies;
+}
---
+++ src/main/java/com/golestan/app/domain/Course/PishniaziCondition.java
@@ -0,0 +1,7 @@
+package com.golestan.app.domain.Course;
+
+/**
+ * Created by mehdithreem on 5/20/2017 AD.
+ */
+public class PishniaziCondition {
+}
