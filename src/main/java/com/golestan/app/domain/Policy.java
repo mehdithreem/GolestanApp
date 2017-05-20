321d01048ef4a13f985af6ba356c6eb188b1c6cf
nahal <mirzanahal@gmail.com>
2017-05-20 14:45:08 +0430

    policy interface

--- src/main/java/com/golestan/app/domain/Policy.java
+++ src/main/java/com/golestan/app/domain/Policy.java
@@ -1,12 +1,19 @@
 package com.golestan.app.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.golestan.app.domain.Student.Student;
+
+import java.util.Date;
 
 /**
  * Created by nahal on 5/20/2017 AD.
  */
 
-public interface Policy {
+public abstract class Policy {
+
+    private Date duration;
+
+    private String name;
+
+    abstract boolean is_justify(Student student);
 
 }
