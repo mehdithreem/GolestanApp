8a4724b49a3086987149cf178985d92ea979e2bc
nahal <mirzanahal@gmail.com>
2017-05-23 09:04:13 +0430

    license

--- src/main/java/com/golestan/app/data/LicenseRepository.java
+++ src/main/java/com/golestan/app/data/LicenseRepository.java
@@ -1,5 +1,12 @@
 package com.golestan.app.data;
 
+import com.golestan.app.domain.Condition;
+import com.golestan.app.domain.Course.Course;
+import com.golestan.app.domain.Student.License;
+import com.golestan.app.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  * Created by mehdithreem on 5/23/2017 AD.
  */
@@ -7,5 +14,24 @@ public class LicenseRepository {
     private static LicenseRepository theRepository = new LicenseRepository();
     public static LicenseRepository getRepository() {
         return theRepository;
+    }
+
+    private LicenseRepository() {
+    }
+
+    public void create(License license) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(license);
+
+        tx.commit();
+        session.close();
+    }
+
+    public License read(Integer id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        License license = session.get(License.class, id);
+        session.close();
+        return license;
     }
 }
--- src/main/java/com/golestan/app/domain/Student/License.java
+++ src/main/java/com/golestan/app/domain/Student/License.java
@@ -16,4 +16,23 @@ public class License {
 
     @Column(name = "title")
     private String title;
+
+    public License() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
